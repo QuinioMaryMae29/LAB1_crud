2d8569c7eea1b90a842a6524c78cf8ac033bb8e9
mary-mae <quiniomarymaea@gmail.com>
2023-09-21 21:32:08 +0800

    up

--- app/Views/products.php
+++ app/Views/products.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <center><h1>Product Listing</h1></center>
+    <center>
+        <table border="5" style="width:60%">
+        <tr>
+            <th>ProductName</th>
+            <th>ProductDescription</th>
+            <th>ProductCategory</th>
+            <th>ProductQuantity</th>
+            <th>ProductPrice</th>
+        </tr>
+        <?php foreach($product as $pr): ?>
+            <tr>
+                <td><?= $pr['ProductName'] ?></td>
+                <td><?= $pr['ProductDescription'] ?></td>
+                <td><?= $pr['ProductCategory'] ?></td>
+                <td><?= $pr['ProductQuantity'] ?></td>
+                <td><?= $pr['ProductPrice'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    </center>
 </body>
 </html>
